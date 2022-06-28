435c29679674f6481ba41a023e746d8d27119ff3
Bruno Henrique <bruno_henrique_pedroso@hotmail.com>
2022-06-27 21:06:32 -0300

    continuando

---
+++ aula11/ex008.js
@@ -0,0 +1,14 @@
+/*var vel = 60.5
+console.log(`A valocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. Multado`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)*/
+
+var pais = 'Angola'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é Estrangeiro')
+}
